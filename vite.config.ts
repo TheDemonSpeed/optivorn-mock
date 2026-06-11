8004b33a65f80fb09f709fd733a79439b8b461fb
TheDemonSpeed <charpeakshay360@gmail.com>
2026-06-12 00:42:05 +0530

    Fix url

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://optivorn-mock-backend.vercel.app',
         changeOrigin: true,
       },
     },
